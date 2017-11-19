3b2cf23cbf02d4e446334fd142af7afd2af9c02b
James Gomis <jamesislike@gmail.com>
2017-11-19 19:49:33 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
+const SiteController = require("./controllers/SiteController.js")
 
 
 // Routes
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,15 @@
 const SiteController = {};
 
+SiteController.index = function(req, resp){
+  resp.render("/")
+}
+
+SiteController.about = function(req, resp){
+  resp.render("/about")
+}
+
+SiteController.contact = functon(req, resp){
+  resp.render("/contact")
+}
 
 module.exports = SiteController;
